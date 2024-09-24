1201481b1d2742e52f932c23663a1253a05a9fc8
maxamx <maxamx@gmail.com>
2024-09-24 17:34:15 -0300

    utilizando o math rand

--- cards/main.go
+++ cards/main.go
@@ -1,9 +1,10 @@
 package main
 
 func main() {
-	//cards := newDeck()
+	cards := newDeck()
+	cards.shuffle()
 	//cards.saveToFile("my_cards")
-	cards := newDeckFromFile("my_cards")
+	//cards := newDeckFromFile("my_cards")
 	cards.print()
 
 }
